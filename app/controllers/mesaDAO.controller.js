3f0a7410b22b6ea7c6694e5f29fe3fda3a4ec860
Victor Rodolfo Gonzalez Galeano <geekpy@hotmail.com>
2021-06-01 11:41:40 -0400

    Before merge with alex

--- app/controllers/mesaDAO.controller.js
+++ app/controllers/mesaDAO.controller.js
@@ -9,108 +9,116 @@
 
 */
 
-const db = require("../models"); // importamos nuestro objeto de la base de datos 
+const db = require("../models"); // importamos nuestro objeto de la base de datos
 const Mesa = db.mesas; // asignamos a una variable nuestro modelo
 const Op = db.Sequelize.Op;
 
 // === Implementación del CRUD ( POST, GET, PUT, DELETE ) ===
 exports.crearMesa = (req, res) => {
-    // Creamos un objeto comentario
-
-    /*  const point = {
-         type: 'Point',
-         coordinates: []
-             ,
-                     crs: { type: 'name', properties: { name: 'EPSG:4326' } }
-     }; */
-
-    const mesa = { // Lo que viene como JSON en la solicitud del postman 
-        nombre: req.body.nombre,
-        posicionX: req.body.posicionX,
-        posicionY: req.body.posicionY,
-        planta: req.body.planta,
-        capacidad: req.body.capacidad,
-        fk_restauranteid: req.body.fk_restauranteid
-    };
-    // Función que guarda la mesa en la base de datos 
-    Mesa.create(mesa).then(data => {
-        res.send(data);
-    }).catch(err => {
-        res.status(500).send({
-            mensaje: err.mensaje || "Ocurrio algun error mientras se crea la mesa."
-        });
+  const mesa = {
+    // Lo que viene como JSON en la solicitud del postman
+    nombre: req.body.nombre,
+    posicionX: req.body.posicionX,
+    posicionY: req.body.posicionY,
+    planta: req.body.planta,
+    capacidad: req.body.capacidad,
+    fk_restauranteid: req.body.fk_restauranteid,
+  };
+  // Función que guarda la mesa en la base de datos
+  Mesa.create(mesa)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        mensaje: err.mensaje || "Ocurrio algun error mientras se crea la mesa.",
+      });
     });
 };
 
 exports.obtenerMesaByID = (req, res) => {
-    const id = req.params.id;
-    Mesa.findByPk(id,{ include: ["mesas_reservas"] }).then(data => {
-        res.send(data);
-    }).catch(err => {
-        res.status(500).send({
-            mensaje: "Error al obtener una mesa con el id=" + id
-        });
+  const id = req.params.id;
+  Mesa.findByPk(id, { include: ["mesas_reservas"] })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        mensaje: "Error al obtener una mesa con el id=" + id,
+      });
     });
 };
 
 exports.obtenerMesas = (req, res) => {
-    Mesa.findAll({ include: ["mesas_reservas"] }).then(data => {
-        res.send(data);
-    }).catch(err => {
-        res.status(500).send({
-            mensaje: err.mensaje || "Ocurrio un error mientras se trata de obtener todas las mesas."
-        });
+  Mesa.findAll({ include: ["mesas_reservas"] })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        mensaje:
+          err.mensaje ||
+          "Ocurrio un error mientras se trata de obtener todas las mesas.",
+      });
     });
 };
 
 exports.actualizarMesaByID = (req, res) => {
-    const id = req.params.id;
-    Mesa.update(req.body, {
-        where: { id: id }
-    }).then(num => {
-        if (num == 1) {
-            res.send({
-                mensaje: "Mesa fue actualizado exitosamente."
-            });
-        } else {
-            res.send({
-                mensaje: `No se pudo actualizar la mesa con id=${id}. Quizas no exista.`
-            });
-        }
-    }).catch(err => {
-        res.status(500).send({
-            mensaje: "Error, no se pudo actualizar la mesa con id=" + id
+  const id = req.params.id;
+  Mesa.update(req.body, {
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          mensaje: "Mesa fue actualizado exitosamente.",
         });
+      } else {
+        res.send({
+          mensaje: `No se pudo actualizar la mesa con id=${id}. Quizas no exista.`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        mensaje: "Error, no se pudo actualizar la mesa con id=" + id,
+      });
     });
 };
 
 exports.borrarMesaByID = (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    Mesa.destroy({
-        where: { id: id }
-    }).then(num => {
-        if (num == 1) {
-            res.send({
-                mensaje: "La mesa fue borrado exitosamente!"
-            });
-        }
-    }).catch(err => {
-        res.status(500).send({
-            mensaje: "No pudo ser borrado la mesa con id=" + id
+  Mesa.destroy({
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          mensaje: "La mesa fue borrado exitosamente!",
         });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        mensaje: "No pudo ser borrado la mesa con id=" + id,
+      });
     });
 };
 
 exports.borrarMesas = (req, res) => {
-    Mesa.destroy({
-        where: {},
-        truncate: false
-    }).then(nums => {
-        res.send({ mensaje: `${nums} Mesas fueron borrados exitosamente!` });
-    }).catch(err => {
-        res.status(500).send({
-            mensaje: err.mensaje || "Algun error ocurrio mientras fueron removidos las mesas."
-        });
+  Mesa.destroy({
+    where: {},
+    truncate: false,
+  })
+    .then((nums) => {
+      res.send({ mensaje: `${nums} Mesas fueron borrados exitosamente!` });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        mensaje:
+          err.mensaje ||
+          "Algun error ocurrio mientras fueron removidos las mesas.",
+      });
     });
 };
--- app/controllers/reservaDAO.controller.js
+++ app/controllers/reservaDAO.controller.js
@@ -5,111 +5,122 @@ const Mesa = db.mesas;
 const Op = db.Sequelize.Op;
 const Cliente = db.clientes;
 
-
 // === Implementación del CRUD ( POST, GET, PUT, DELETE ) ===
 exports.crearReserva = (req, res) => {
-     //para obtener el id del cliente
-        Cliente.findOne({
-            where: {
-                cedula: req.body.clienteCI
-            }
-        }).then(data => {
-            if(data){
-                console.log(data.id); 
-            const reserva = { // Lo que viene como JSON en la solicitud del postman 
-                fecha: req.body.fecha,
-                cantidadSolicitada: req.body.cantidadSolicitada,
-                fk_restauranteid: req.body.fk_restauranteid,
-                fk_mesaid: req.body.fk_mesaid,
-                fk_clienteid: data.id,
-                horaInicio: req.body.hora_inicio,
-                horaFin: req.body.hora_fin
-            };
-                    // Función que guarda la mesa en la base de datos 
-            Reserva.create(reserva).then(data => {
-                res.send(data);
-            }).catch(err => {
-                res.status(500).send({
-                    mensaje: err.mensaje || "Ocurrio algun error mientras se crea la reserva."
-                });
-            });
-            }else{
-                res.status(204).send();
-            }
-        });    
+  //para obtener el id del cliente
+  Cliente.findOne({
+    where: {
+      cedula: req.body.clienteCI,
+    },
+  }).then((data) => {
+    if (data) {
+      console.log(data.id);
+      const reserva = {
+        // Lo que viene como JSON en la solicitud del postman
+        fecha: req.body.fecha,
+        cantidadSolicitada: req.body.cantidadSolicitada,
+        fk_restauranteid: req.body.fk_restauranteid,
+        fk_mesaid: req.body.fk_mesaid,
+        fk_clienteid: data.id,
+        horaInicio: req.body.hora_inicio,
+        horaFin: req.body.hora_fin,
+      };
+      // Función que guarda la mesa en la base de datos
+      Reserva.create(reserva)
+        .then((data) => {
+          res.send(data);
+        })
+        .catch((err) => {
+          res.status(500).send({
+            mensaje:
+              err.mensaje || "Ocurrio algun error mientras se crea la reserva.",
+          });
+        });
+    } else {
+      res.status(204).send();
+    }
+  });
 };
 
 exports.mesasDisponibles = (req, res) => {
-    const restauranteid = req.query.restaurante;
-    const fecha = req.query.fecha;
-    const hora_Inicio = req.query.hora_inicio;
-    const hora_Fin = req.query.hora_fin;
-    
-    Reserva.findAll({
-        where:{ 
-            fk_restauranteid:restauranteid,
-            fecha: fecha,
-            [Op.or]:[
-                { horaInicio: hora_Inicio }, 
-                { horaFin:hora_Fin},
-                { horaInicio:
-                    {
-                        [Op.gt]:hora_Inicio,
-                        [Op.lt]:hora_Fin
-                    }
-                },
-                { horaFin:
-                    {
-                        [Op.gt]:hora_Inicio,
-                        [Op.lt]:hora_Fin
-                    }
-                }
-            ]
-        }
-    }).then(data =>  {
-        let total_mesas =  Mesa.findAll({
-            where:{
-                fk_restauranteid:restauranteid
-            }
-        });
-        
-        let mesas_disponibles = [];
+  const restauranteid = req.query.restaurante;
+  const fecha = req.query.fecha;
+  const hora_Inicio = req.query.hora_inicio;
+  const hora_Fin = req.query.hora_fin;
 
-        total_mesas.then(tm=>{
-            tm.forEach(mesa => {
-                if (!data.some(reserva => reserva.fk_mesaid == mesa.id)){
-                    mesas_disponibles = [...mesas_disponibles, mesa];
-                }
-            });
-            res.send(mesas_disponibles);
+  Reserva.findAll({
+    where: {
+      fk_restauranteid: restauranteid,
+      fecha: fecha,
+      [Op.or]: [
+        { horaInicio: hora_Inicio },
+        { horaFin: hora_Fin },
+        {
+          horaInicio: {
+            [Op.gt]: hora_Inicio,
+            [Op.lt]: hora_Fin,
+          },
+        },
+        {
+          horaFin: {
+            [Op.gt]: hora_Inicio,
+            [Op.lt]: hora_Fin,
+          },
+        },
+      ],
+    },
+  })
+    .then((data) => {
+      let total_mesas = Mesa.findAll({
+        where: {
+          fk_restauranteid: restauranteid,
+        },
+      });
+
+      let mesas_disponibles = [];
+
+      total_mesas.then((tm) => {
+        tm.forEach((mesa) => {
+          if (!data.some((reserva) => reserva.fk_mesaid == mesa.id)) {
+            mesas_disponibles = [...mesas_disponibles, mesa];
+          }
         });
-    }).catch(err => {
-        res.status(500).send({
-            mensaje: err.mensaje || "Ocurrio un error mientras se trata de obtener todas las mesas."
-        });
+        res.send(mesas_disponibles);
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        mensaje:
+          err.mensaje ||
+          "Ocurrio un error mientras se trata de obtener todas las mesas.",
+      });
     });
 };
 
 exports.listaReservas = (req, res) => {
-    const restaurante = req.query.restaurante;
-    const fecha = req.query.fecha;
-    //const cliente = req.query.cliente;
-
-    Reserva.findAll({
-        where:{ 
-            fk_restauranteid:restaurante,
-            fecha:fecha,
-            //fk_clienteid: {[Op.or]:cliente}
-            },
-        order: [
-                ['horaInicio', 'ASC'],
-                ['fk_mesaid', 'ASC'],
-            ]
-    }).then(data => {
-        res.send(data);
-    }).catch(err => {
-        res.status(500).send({
-            mensaje: err.mensaje || "Ocurrio un error mientras se trata de obtener todas las reservas."
-        });
+  const restaurante = req.query.restaurante;
+  const fecha = req.query.fecha;
+  //const cliente = req.query.cliente;
+  
+  Reserva.findAll({
+    where: {
+      fk_restauranteid: restaurante,
+      fecha: fecha,
+      //fk_clienteid: {[Op.or]:cliente}
+    },
+    order: [
+      ["horaInicio", "ASC"],
+      ["fk_mesaid", "ASC"],
+    ],
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        mensaje:
+          err.mensaje ||
+          "Ocurrio un error mientras se trata de obtener todas las reservas.",
+      });
     });
 };
